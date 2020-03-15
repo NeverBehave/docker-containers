2abe481c68df932bb7ee965ef9966fb80564b550
NeverBehave <gayhub@never.pet>
2020-03-15 16:15:44 -0400

    fix generator script

--- FoldingAtHome/firstrun.sh
+++ FoldingAtHome/firstrun.sh
@@ -9,22 +9,25 @@ TEMPLATE_PATH="/root/config.xml"
 if [ -f ${CONFIG_PATH} ]; then
   
   echo "[I] Using existing config file."
+  cat ${CONFIG_PATH}
   
   else
 
   echo "[I] Creating config from template."
   
-  template=$(< ${TEMPLATE_PATH})
-  printf "$template" \ 
+  TEMPLATE_CONFIG=$(cat ${TEMPLATE_PATH})
+
+  APPLIED_TEMPLATE=$(printf "$TEMPLATE_CONFIG" \
               "$GPU_STATUS" "$ALLOW_SUBNET_HTTP" "$PASSWORD" \
               "$TEAM_ID" "$USER_ID" "$PASSKEY" \
-              "$ALLOW_SUBNET_WEB" "$POWER" > ${CONFIG_PATH}
+              "$ALLOW_SUBNET_WEB" "$POWER")
 
   echo "[I] Config Created"
-fi
+  echo "$APPLIED_TEMPLATE"
 
-echo "[I] Current Config"
-cat ${CONFIG_PATH}
+  echo "[I] Writing Config"
+  echo "$APPLIED_TEMPLATE" > ${CONFIG_PATH}
+fi
 
 echo "[I] Setting file permission"
 chown nobody:users ${CONFIG_PATH}
